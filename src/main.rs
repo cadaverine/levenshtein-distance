5381c006b3dcd7f4899a334c2e9016344cf2ddaa
cadaverine <kennville@gmail.com>
2019-02-13 22:44:19 +0300

    feat: add 'get_levenstein_distance' function base

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::cmp;
+
+#[allow(dead_code)]
+fn get_levenstein_distance(first: &str, second: &str) -> usize {
+    let removeCost = 1;
+    let insertCost = 1;
+    let replaceCost = 1;
+
+    let firstLength = first.len();
+    let secondLength = second.len();
+
+    let maxDistance = cmp::max(firstLength, secondLength);
+    maxDistance
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let firstString = "toggle";
+    let secondString = "google";
+
+    println!("{}", get_levenstein_distance(firstString, secondString));
 }
