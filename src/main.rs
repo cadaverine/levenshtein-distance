41dde95fd5b4c3f1e8a306a1f01da92389ab09ec
cadaverine <kennville@gmail.com>
2019-03-03 18:26:38 +0300

    feat(main.rs): add console input to main function

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,22 @@
 mod edit_distance;
+
+use std::io;
 use edit_distance::*;
 
 fn main() {
-    let first_string = "овал";
-    let second_string = "кровля";
+    loop {
+        let mut first_string = String::new();
+        let mut second_string = String::new();
 
-    println!(
-        "{:?}",
-        get_levenshtein_distance(first_string, second_string)
-    );
+        println!("Введите первую строку:");
+        let _ = io::stdin().read_line(&mut first_string);
+        
+        println!("Введите вторую строку:");
+        let _ = io::stdin().read_line(&mut second_string);
+
+        println!(
+            "{:?}",
+            get_levenshtein_distance(&first_string, &second_string)
+        );
+    }
 }
